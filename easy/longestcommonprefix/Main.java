3ea1ce92d52e033d12778d8c2c01054a7d7f129f
StevenNguyenUIT <143907704+StevenNguyenUIT@users.noreply.github.com>
2023-10-02 21:35:03 -0500

    update ignore

--- easy/longestcommonprefix/Main.java
+++ easy/longestcommonprefix/Main.java
@@ -2,6 +2,13 @@ package easy.longestcommonprefix;
 
 public class Main {
     public static void main(String[] args) {
+        String[] strs = new String[]{"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+        System.out.println(strs.length);
+    }
 
+    private static String longestCommonPrefix(String[] strs) {
+        String result = "";
+        return result;
     }
 }
